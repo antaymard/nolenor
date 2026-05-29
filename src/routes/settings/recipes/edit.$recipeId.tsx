115f51bb13ced20db0876c7e5907f1b46d60b7d9
antaymard <antaymard@gmail.com>
2026-05-30 01:21:16 +0200

    oki !

--- src/routes/settings/recipes/edit.$recipeId.tsx
+++ src/routes/settings/recipes/edit.$recipeId.tsx
@@ -8,6 +8,14 @@ import { api } from "@/../convex/_generated/api";
 import { useNavigate } from "@tanstack/react-router";
 import TextInput from "@/components/ts-form/TextInput";
 
+import "@blocknote/core/fonts/inter.css";
+import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/shadcn";
+import { Block, BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import "@blocknote/shadcn/style.css";
+import { useMemo } from "react";
+import useRichQuery from "@/components/utils/useRichQuery";
+
 export const Route = createFileRoute("/settings/recipes/edit/$recipeId")({
   component: RouteComponent,
 });
@@ -16,6 +24,11 @@ function RouteComponent() {
   const upsertRecipe = useMutation(api.recipes.upsert);
   const navigate = useNavigate();
   const { recipeId } = Route.useParams();
+
+  const { data: recipe, isSuccess } = useRichQuery(
+    api.recipes.read,
+    recipeId === "new" ? "skip" : { recipeId: recipeId as Id<"recipes"> },
+  );
 
   const form = useForm({
     defaultValues: {
@@ -56,17 +69,24 @@ function RouteComponent() {
     },
   });
 
+  const editor = useMemo(() => {
+    if (recipeId === "new") {
+      return BlockNoteEditor.create();
+    }
+    if (isSuccess && recipe) {
+      form.setFieldValue("name", recipe.name);
+      form.setFieldValue("content", recipe.content);
+      return BlockNoteEditor.create({
+        initialContent: JSON.parse(recipe.content) as PartialBlock[],
+      });
+    }
+  }, [recipeId, isSuccess]);
+
   return (
-    <form
-      className="max-w-4xl mx-auto"
-      onSubmit={(e) => {
-        e.preventDefault();
-        form.handleSubmit();
-      }}
-    >
+    <div className="max-w-4xl mx-auto">
       <div className="flex items-center justify-between">
         <h1 className="text-xl font-bold">New recipe</h1>
-        <Button type="submit">Save</Button>
+        <Button onClick={() => form.handleSubmit()}>Save</Button>
       </div>
 
       <div className="mt-8 bg-slate-50 rounded p-2 space-y-2 ">
@@ -77,7 +97,18 @@ function RouteComponent() {
           required
           inputClassName="bg-white"
         />
+        {editor ? (
+          <BlockNoteView
+            theme="light"
+            editor={editor}
+            onChange={() => {
+              form.setFieldValue("content", JSON.stringify(editor.document));
+            }}
+          />
+        ) : (
+          <div>Loading editor...</div>
+        )}
       </div>
-    </form>
+    </div>
   );
 }

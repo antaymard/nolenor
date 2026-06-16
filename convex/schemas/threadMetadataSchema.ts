85bc035f0ef7053e05e679e7a89a69bc0bd232c9
antaymard <antaymard@gmail.com>
2026-06-16 16:09:23 +0200

    remove some console.log

--- convex/schemas/threadMetadataSchema.ts
+++ convex/schemas/threadMetadataSchema.ts
@@ -5,6 +5,7 @@ const threadMetadataValidator = v.object({
   userId: v.id("users"),
   canvasId: v.id("canvases"),
   totalUsageUsd: v.number(),
+  touchedNodeDataIds: v.optional(v.array(v.id("nodeDatas"))), // Nodedata that have been modified during the thread, by the agent
   agentName: v.string(),
   lastMessageTime: v.optional(v.number()),
   roundsNb: v.optional(v.number()),

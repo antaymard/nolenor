d44c3a0d8bfd7634c124eb509948580d553391b6
antaymard <antaymard@gmail.com>
2026-07-15 16:52:35 +0200

    Update BlocknoteNode.tsx

--- src/components/nodes/prebuilt-nodes/BlocknoteNode.tsx
+++ src/components/nodes/prebuilt-nodes/BlocknoteNode.tsx
@@ -164,7 +164,7 @@ function BlocknoteNode(xyNode: Node) {
                   </div>
                 ) : html ? (
                   <div
-                    className="h-full min-h-0 overflow-y-auto p-4 nodrag select-none bn-readonly-container"
+                    className={`h-full min-h-0 overflow-y-auto p-4 select-none bn-readonly-container${xyNode.selected ? " nodrag nowheel" : ""}`}
                     dangerouslySetInnerHTML={{ __html: html }}
                   />
                 ) : null}
